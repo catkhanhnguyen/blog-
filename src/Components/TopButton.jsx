0c4f4b7626a14a50163cd4ee1c7e7efb0b1396d7
Khanh Nguyen Le Cat <nlckhanh-batch43@sdc.edu.vn>
2024-03-01 17:29:22 +0700

    update

--- src/Components/TopButton.jsx
+++ src/Components/TopButton.jsx
@@ -1,12 +1,12 @@
 
 const TopButton = () => {
   const handleScrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0 });
   };
 
   return (
     <div
-      className="fixed flex bottom-10 right-10 w-12 h-12 bg-red-500 justify-center items-center rounded-full cursor-pointer text-white text-l font-bold transition duration-2 hover:bg-red-600"
+      className="fixed flex bottom-10 right-10 w-12 h-12 bg-red-500 justify-center items-center rounded-full cursor-pointer text-white text-l font-bold transition duration-10 hover:bg-red-600"
       onClick={handleScrollToTop}
     >
       TOP
